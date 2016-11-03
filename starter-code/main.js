5f63a18d38abf74d0cc356aabc5cb951d78ab46a
Ed Compton <Ed Compton>
2016-11-03 11:22:07 +0000

    added javascript functionality

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,11 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";;
+
+if (cardTwo === cardFour) {
+	console.log(alert("You found a match!"));
+} else if (cardOne === cardThree) {
+	console.log(alert("You found a match!"));
+} else {
+	console.log(alert("Sorry, try again"));
+}
