3640d8902bc1ad9dca9d571507907fd069d70611
Ed Compton <Ed Compton>
2016-11-16 10:35:32 +0000

    changes made

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,10 +3,24 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";;
 
-if (cardTwo === cardFour) {
-	console.log(alert("You found a match!"));
-} else if (cardOne === cardThree) {
-	console.log(alert("You found a match!"));
-} else {
-	console.log(alert("Sorry, try again"));
+//if (cardTwo === cardFour) {
+//	console.log(alert("You found a match!"));
+//} else if (cardOne === cardThree) {
+//	console.log(alert("You found a match!"));
+//} else {
+//	console.log(alert("Sorry, try again"));
+//}
+
+var gameBoard = document.getElementById('game-board');
+
+var newCard = document.createElement('div');
+
+var max = 3;
+
+var createCards = window.onload = function() {
+	for (x = 0; x <= max; x++) {
+var newCard = document.createElement('div');
+newCard.className = 'card';
+gameBoard.appendChild(newCard);
+}
 }
