a89b248024f26ef8fbf1738e9ee8db7bcb934613
Ed Compton <Ed Compton>
2016-11-20 14:51:20 +0000

    final code

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,26 +1,56 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";;
-
-//if (cardTwo === cardFour) {
-//	console.log(alert("You found a match!"));
-//} else if (cardOne === cardThree) {
-//	console.log(alert("You found a match!"));
-//} else {
-//	console.log(alert("Sorry, try again"));
-//}
-
 var gameBoard = document.getElementById('game-board');
 
-var newCard = document.createElement('div');
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-var max = 3;
+var clearBoard = function () {
+	var wipe = gameBoard;
+	while (wipe.firstChild) {
+		wipe.removeChild(wipe.firstChild);
+	}
+}
 
-var createCards = window.onload = function() {
-	for (x = 0; x <= max; x++) {
-var newCard = document.createElement('div');
-newCard.className = 'card';
-gameBoard.appendChild(newCard);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+		gameBoard.appendChild(cardElement);
+		gameBoard.appendChild(cardElement);
+	}
 }
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='King.png'>";
+	} else {
+		this.innerHTML = "<img src='Queen.png'>";
+	} 
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
 }
+
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+		clearBoard();
+		createBoard();
+	} else {
+		alert("Sorry, try again");
+		clearBoard();
+		createBoard();
+	}
+}
+
+
+
+createBoard();
+
+
+//		for (var i = 0; i < cardsInPlay.length; i++) {
+//				document.getAttribute('data-card').innerHTML = "";	
